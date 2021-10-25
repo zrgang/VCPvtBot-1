64b2120e47e1d710decc49b01380238850bf2741
Aksr aashish <76800327+aksr-aashish@users.noreply.github.com>
2021-10-25 08:33:46 -0700

    Update config.py

--- config.py
+++ config.py
@@ -7,13 +7,13 @@ que = {}
 SESSION_NAME = getenv("SESSION_NAME", "session")
 BOT_TOKEN = getenv("BOT_TOKEN", "")
 BOT_USERNAME = getenv("BOT_USERNAME", "")
-UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "LaylaList")
+UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "vexana_support")
 admins = {}
 API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH", "")
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "10"))
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ !").split())
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1461968113").split()))
 
 
 
